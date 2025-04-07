8ca89155146bebd81923130553fb791fcfb8da7e
Jebbs-dev <ojebiyifulness@gmail.com>
2025-04-07 10:00:52 +0100

    config: configure image component

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  transpilePackages: ["geist"],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        port: "",
+        // path: "/image/upload",
+        search: "",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
