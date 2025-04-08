de3ed3746801cdc1e6b8267911823118ab7eed4a
Jebbs-dev <ojebiyifulness@gmail.com>
2025-04-08 12:21:06 +0100

    config: add config for shadcn ui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,33 +1,79 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: [
-    // './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    // './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    'src/**/*.{js,ts,jsx,tsx,mdx}',
+    darkMode: ['class'],
+    content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'hero': "linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('./components/images/meals3.jpg')",
-      },
-      screens: {
-        'dw': '240px',
-        'int': '950px',
-      },
-      colors: {
-        'brownie': '#fe9446',
-        'browned': '#98582a',
-        'hair': '#403227',
-        'base': '#fff4f2',
-        'origin': '#fb9333',
-      },
-      gridTemplateColumns: {
-        'footer': '2fr repeat(2, 1.2fr) 1.5fr  2fr',
-        'cart': '1fr 2fr 1fr'
-      }
-    }
+  	extend: {
+  		backgroundImage: {
+  			hero: "linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('/images/meals3.jpg')"
+  		},
+  		screens: {
+  			dw: '240px',
+  			int: '950px'
+  		},
+  		colors: {
+  			brownie: '#fe9446',
+  			browned: '#98582a',
+  			hair: '#403227',
+  			base: '#fff4f2',
+  			origin: '#fb9333',
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			}
+  		},
+  		gridTemplateColumns: {
+  			footer: '2fr repeat(2, 1.2fr) 1.5fr  2fr',
+  			cart: '1fr 2fr 1fr'
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 }
 export default config
