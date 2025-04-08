e4bc44135f4effa057253a822bd243601730fa34
Jebbs-dev <ojebiyifulness@gmail.com>
2025-04-08 12:35:04 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,25 @@
-import {CtaSection} from "@/modules/landing/cta/cta";
+"use client";
+
+import { CtaSection } from "@/modules/landing/cta/cta";
+import Faqs from "@/modules/landing/faqs";
+import GetStarted from "@/modules/landing/get-started";
 import { HeroSection } from "@/modules/landing/hero";
 import { Menu } from "@/modules/landing/menu/menu";
+import Navigation from "@/modules/landing/navigation";
+import Reservation from "@/modules/landing/reservation/reservation";
 import { Services } from "@/modules/landing/services/services";
 
 export default function Home() {
+  const cartItems: any = [];
+
+  // localStorage.setItem("cartItem", JSON.stringify(cartItems));
+
   return (
-    <main className="">
+    <main>
       <HeroSection />
-      <Services/>
-      <CtaSection/>
-      <Menu/>
+      <Services />
+      <CtaSection />
+      <GetStarted />
     </main>
   );
 }
