f823e54b421e614c50189d15f7dd416d512c7ffd
Jebbs-dev <ojebiyifulness@gmail.com>
2025-04-23 23:48:01 +0100

    Update payment-page.tsx

--- src/modules/checkout/components/payment-page.tsx
+++ src/modules/checkout/components/payment-page.tsx
@@ -1,12 +1,78 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import DeliveryInformation from "./delivery-information";
 import PickupInformation from "./pickup-information";
+import { useLoadScript } from "@react-google-maps/api";
+import { useFetchVendorStoreById } from "@/modules/shop/queries/fetch-vendor-store-by-id";
+import { useCartViewStore } from "@/store/cart-data";
+import useAuthStore from "@/store/auth";
+import { useFetchUserInformation } from "@/modules/user/queries/fetch-user-information";
 
 const PaymentPage = () => {
+  const { openStoreId } = useCartViewStore();
+  const { user } = useAuthStore();
+  const { data: storeInformation } = useFetchVendorStoreById(
+    String(openStoreId)
+  );
+  const { data: userInformation } = useFetchUserInformation(String(user?.id));
+
   const [toggleDeliveryOption, setToggleDeliveryOption] = useState<
     "delivery" | "pickup"
   >("delivery");
+
+  
+
+  const libraries = ["places"];
+  const [routeInfo, setRouteInfo] = useState<{
+    distance: string;
+    duration: string;
+    summary: string;
+  } | null>(null);
+
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY!,
+    libraries: libraries as any,
+  });
+
+  const getRoute = (origin: string, destination: string) => {
+    if (!window.google) return;
+
+    const directionsService = new window.google.maps.DirectionsService();
+
+    directionsService.route(
+      {
+        origin,
+        destination,
+        travelMode: window.google.maps.TravelMode.DRIVING,
+      },
+      (result, status) => {
+        if (
+          status === window.google.maps.DirectionsStatus.OK &&
+          result?.routes?.length
+        ) {
+          const route = result.routes[0];
+          const leg = route.legs[0];
+          setRouteInfo({
+            distance: leg.distance?.text || "",
+            duration: leg.duration?.text || "",
+            summary: route.summary,
+          });
+
+          console.log("Full Directions Result:", result);
+        } else {
+          console.error("Directions request failed:", status);
+        }
+      }
+    );
+  };
+  useEffect(() => {
+    getRoute(storeInformation?.address, userInformation?.address);
+  }, [storeInformation?.address, userInformation?.address]);
+
+  console.log(routeInfo);
+
+  const thresholdInKm = 15;
+  const distanceToVendor = parseFloat(String(routeInfo?.distance));
 
   return (
     <>
@@ -29,10 +95,10 @@ const PaymentPage = () => {
             </TabsTrigger>
           </TabsList>
           <TabsContent value="delivery" className="h-[73vh]">
-            <DeliveryInformation />
+            <DeliveryInformation thresholdInKm={thresholdInKm} distanceToVendor={distanceToVendor} userId={String(user?.id)} storeId={storeInformation?.id} />
           </TabsContent>
           <TabsContent value="pickup">
-            <PickupInformation />
+            <PickupInformation thresholdInKm={thresholdInKm} distanceToVendor={distanceToVendor} />
           </TabsContent>
         </Tabs>
       </div>
