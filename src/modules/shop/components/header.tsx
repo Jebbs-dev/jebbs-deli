27696bdc7463440d051f86460b9792e27547116a
Jebbs-dev <ojebiyifulness@gmail.com>
2025-04-24 23:06:06 +0100

    feat: add places autocomplete to header

--- src/modules/shop/components/header.tsx
+++ src/modules/shop/components/header.tsx
@@ -15,19 +15,43 @@ import {
   DropdownMenuShortcut,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { FileClock, LogOut, Settings } from "lucide-react";
+import {
+  ChevronDown,
+  ChevronRight,
+  FileClock,
+  LogOut,
+  MapPin,
+  Settings,
+} from "lucide-react";
 import useAuthStore from "@/store/auth";
 import { useRouter } from "next/navigation";
 import { useFetchCart } from "../cart/queries/fetch-cart";
 import useCartStore from "@/store/cart";
 import { useCartViewStore } from "@/store/cart-data";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import PlacesAutocomplete from "@/components/map/places-autocomplete";
+import { useUpdateUser } from "@/modules/user/mutations/update-user";
+import { useFetchUserInformation } from "@/modules/user/queries/fetch-user-information";
+
+export type Prediction = google.maps.places.AutocompletePrediction;
 
 const Header = () => {
-  // const [isOpen, setIsOpen] = useState(false);
+
+
+  const [input, setInput] = useState("");
+  const [predictions, setPredictions] = useState<Prediction[]>([]);
 
   const { isOpen, setIsOpen } = useCartViewStore();
 
   const { user, isLoggedIn, logout } = useAuthStore();
+  const { data: userInformation } = useFetchUserInformation(String(user?.id));
+
   const { totalItems } = useCartStore();
   const router = useRouter();
 
@@ -40,17 +64,45 @@ const Header = () => {
       ?.flatMap((group) => group.cartItems)
       .reduce((sum, item) => sum + item.quantity, 0) || 0;
 
+  console.log("from header", input);
+
   return (
     <>
-      <CartSheet isOpen={isOpen} setIsOpen={setIsOpen} />
+      <CartSheet />
       <header className="border-b border-x-gray-100 fixed w-full left-0 z-50 bg-white">
         <div className="w-full bg-white flex justify-between h-[75px] md:h-[81px] px-4 md:px-10 mx-auto py-4">
-          <div>
-            <Link href="/shop">
+          <div className="flex md:flex-row gap-10">
+            <Link href="/shop" className="flex items-center">
               <h1 className="mx-auto md:top-10 md:left-10 text-black text-2xl md:text-3xl">
                 deli<span className="text-orange-400">.</span>
               </h1>
             </Link>
+
+            <Dialog>
+              <DialogTrigger className="flex flex-row items-center gap-2">
+                <MapPin className="text-green-600" size={18} />
+                <p className="text-sm">{userInformation?.address} </p>
+                <ChevronDown size={18} />
+              </DialogTrigger>
+              <DialogContent className="h-[500px] p-10">
+                <DialogHeader className="flex flex-col gap-5">
+                  <DialogTitle className="text-lg md:text-2xl text-left font-semibold text-orange-400">
+                    Delivery Address
+                  </DialogTitle>
+                  <div className="relative text-gray-600 w-full">
+                    <span className="absolute top-0 bottom-0 left-2 flex items-center">
+                      <MapPin className="text-green-600" />
+                    </span>
+                    <PlacesAutocomplete
+                      input={input}
+                      setInput={setInput}
+                      predictions={predictions}
+                      setPredictions={setPredictions}
+                    />
+                  </div>
+                </DialogHeader>
+              </DialogContent>
+            </Dialog>
           </div>
           <div className="flex items-center space-x-5">
             <div className="relative text-gray-600  hidden md:flex">
@@ -90,7 +142,9 @@ const Header = () => {
               <Button
                 className="relative bg-orange-400 flex items-center justify-center rounded-full w-[30px] h-[30px] md:w-[45px] md:h-[45px] text-white shadow-indigo-500/40"
                 aria-label="Cart"
-                onClick={() => setIsOpen(true)}
+                onClick={() => {
+                  setIsOpen(true)
+                }}
               >
                 <svg
                   stroke="currentColor"
@@ -175,6 +229,40 @@ const Header = () => {
                 </DropdownMenuContent>
               </DropdownMenu>
             </div>
+          </div>
+        </div>
+        <div className="flex justify-center mb-2 md:hidden">
+          <div className="relative text-gray-600">
+            <span className="absolute top-0.5 bottom-0 left-0 flex items-center">
+              <Button
+                type="submit"
+                className="bg-transparent hover:bg-transparent"
+              >
+                <svg
+                  width="16"
+                  height="16"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M10 17C13.866 17 17 13.866 17 10C17 6.13401 13.866 3 10 3C6.13401 3 3 6.13401 3 10C3 13.866 6.13401 17 10 17Z"
+                    stroke="#0C513F"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  ></path>
+                  <path
+                    d="M15 15L21 21"
+                    stroke="#0C513F"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  ></path>
+                </svg>
+              </Button>
+            </span>
+            <Input className="pl-10 w-80 py-2" />
           </div>
         </div>
       </header>
