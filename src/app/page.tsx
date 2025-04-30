526e53e02b708a162e2d9884790bcbb951f06a35
Jebbs-dev <ojebiyifulness@gmail.com>
2025-04-30 16:56:51 +0100

    chore: remove unused routes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,9 @@
 "use client";
 
 import { CtaSection } from "@/modules/landing/cta/cta";
-import Faqs from "@/modules/landing/faqs";
 import GetStarted from "@/modules/landing/get-started";
 import { HeroSection } from "@/modules/landing/hero";
-import { Menu } from "@/modules/landing/menu/menu";
-import Navigation from "@/modules/landing/navigation";
-import Reservation from "@/modules/landing/reservation/reservation";
+
 import { Services } from "@/modules/landing/services/services";
 
 export default function Home() {
