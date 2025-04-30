8f30988f4eb96e5f7982b1a423aaeaddc79c7064
Jebbs-dev <ojebiyifulness@gmail.com>
2025-04-30 16:54:51 +0100

    feat: disable queries before login

--- src/modules/checkout/queries/fetch-order-by-userId.ts
+++ src/modules/checkout/queries/fetch-order-by-userId.ts
@@ -1,12 +1,17 @@
+import useAuthStore from "@/store/auth";
 import api from "@/utils/api";
-import { useQuery } from "@tanstack/react-query";
+import { skipToken, useQuery } from "@tanstack/react-query";
 
 export const useFetchOrders = (userId: string) => {
+  const { isLoggedIn } = useAuthStore();
+
   return useQuery({
     queryKey: ["orders", userId],
-    queryFn: async () => {
-      const response = await api.get(`/orders/${userId}`);
-      return response.data;
-    },
+    queryFn: isLoggedIn
+      ? async () => {
+          const response = await api.get(`/orders/${userId}`);
+          return response.data;
+        }
+      : skipToken,
   });
 };
