1290b29a7fe46a2f1da9c4035a392c6d07100629
Jebbs-dev <ojebiyifulness@gmail.com>
2025-05-14 08:43:06 +0100

    chore: remove outdated code

--- src/modules/landing/cta/cta.tsx
+++ src/modules/landing/cta/cta.tsx
@@ -116,46 +116,6 @@ export const CtaSection = () => {
           ))}
         </div>
       </motion.div>
-      {/* <div className="grid int:h-[100vh] max-w-[90vw] mx-auto">
-        <div className="grid md:auto-cols-fr gap-5 items-center md:grid-cols-2">
-          <div className="">
-            <img src="/images/HeroImage.png" alt="Woman eating fruits" />
-          </div>
-          <div className="flex flex-col p-5">
-            <h1 className="font-bold text-4xl md:text-5xl leading-10 md:leading-[65px] text-hair font-sans py-3">
-              We provide the best service for our customers.
-            </h1>
-            <p className="text-1xl md:text-2xl opacity-40">
-              Explore a World of Culinary Delights at Your Fingertips - Order
-              Now for a Gastronomic Journey!.
-            </p>
-            <div className="max-w-[23vw] grid grid-cols-1 int:grid-cols-2 gap-4 grid-flow-row my-5 py-2">
-              {widgets.map((widget) => (
-                <div
-                  className="w-40 h-10 flex flex-row items-center"
-                  key={widget.title}
-                >
-                  <span
-                    className="rounded-full bg-rose-100 w-10 h-10 flex items-center justify-around"
-                    style={{ backgroundColor: widget.color }}
-                  >
-                    <Image src={widget.image} height={24} width={24} alt="" />{" "}
-                  </span>
-                  <p className="ml-2 font-semibold">{widget.title}</p>
-                </div>
-              ))}
-            </div>
-            <div className="flex flex-row my-3 py-3 md:w-auto">
-              <button
-                type="button"
-                className="rounded-md px-4 py-2 w-full md:w-auto  border-solid border-[1px] text-white border-orange-400 bg-orange-400 hover:bg-orange-500 hover:text-white hover:border-orange-500"
-              >
-                Explore more
-              </button>
-            </div>
-          </div>
-        </div>
-      </div> */}
     </div>
   );
 };
--- src/modules/landing/services/services.tsx
+++ src/modules/landing/services/services.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, {  useState } from "react";
+import React, { useState } from "react";
 
 import Image from "next/image";
 import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/components/ui/tabs";
@@ -87,73 +87,6 @@ export const Services = () => {
             </TabsContent>
           </Tabs>
         </div>
-        {/* <div className="flex flex-row justify-between">
-          <h2 className="text-3xl md:text-4xl font-bold">
-            Order Your Favourite Dishes
-          </h2>
-          <div className="flex flex-row">
-            <button
-              className="flex rounded-full h-10 w-10 items-center justify-around bg-white text-orange-400 mr-2"
-              onClick={slideLeft}
-            >
-              <FaAngleLeft className="align-middle" />
-            </button>
-            <button
-              className="flex rounded-full h-10 w-10 items-center justify-around bg-orange-400 text-white"
-              onClick={slideRight}
-            >
-              <FaAngleRight />
-            </button> */}
-        {/* Used flex, items-center and justify around to center the icon inside the div circle */}
-        {/* </div>
-        </div> */}
-        {/* grid grid-cols-1 int:grid-cols-4 md:grid-cols-2 auto-cols-auto gap-5 md:justify-between */}
-        {/* <div className="py-6 relative flex">
-          <div
-            id="slider"
-            className="w-full py-6 gap-4 auto-rows-fr overflow-x-scroll scroll whitespace-nowrap scroll-smooth scrollbar-hide space-x-4"
-          >
-            {Meals.map((meal) => (
-              <div
-                className="w-full int:w-1/4 md:max-w-[310px] h-full p-4 items-center text-center bg-white bg-opacity-70 rounded-[3%] hover:rounded-[3%] hover:shadow-xl hover:scale-105 box-border hover:ease-in-out duration-300 inline-block"
-                key={meal.id}
-              >
-                <button
-                  // id="favourite"
-                  onClick={() => addToFaveHandler(meal)}
-                  className="flex rounded-full h-10 w-10 items-center justify-around bg-orange-200 text-white hover:bg-orange-400 active:bg-orange-400 mb-1"
-                >
-                  <FaHeart
-                    className="w-5 h-5 text-left"
-                    style={{ color: "#FFF" }}
-                  />{" "}
-                </button>
-                <div className=" flex flex-col justify-between space-y-1">
-                  <Image
-                    width={150}
-                    height={150}
-                    src={meal.image}
-                    alt=""
-                    className="mx-auto mb-2"
-                  />
-                  <h3 className="text-[20px] font-semibold">{meal.name}</h3>
-                  <p className="text-orange-400 font-semibold">
-                    ${meal.price.toFixed(2)}
-                  </p>
-                  <p>{meal.description}</p>
-
-                  <button
-                    type="button"
-                    className="rounded-sm border font-medium border-orange-400 py-1 px-3 text-orange-400 hover:bg-orange-400 hover:border-orange-400 hover:text-white"
-                    onClick={() => addToCartHandler(meal)}
-                  >
-                    Add to cart
-                  </button>
-                </div>
-              </div>
-            ))}
-          </div>
-        </div> */}
       </div>
     </div>
   );
