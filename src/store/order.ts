18271eb6e4b1c94218fc6233ee149d7877404f54
Jebbs-dev <ojebiyifulness@gmail.com>
2025-08-15 23:37:56 +0100

    feat: add state store for order data and payment

---
+++ src/store/order.ts
@@ -0,0 +1,28 @@
+import { Order } from "@/types/types";
+import { create } from "zustand";
+
+interface PaymentInitialisePayload {
+  Payment: Record<string, any>;
+  access_code: string;
+  authorization_url: string;
+}
+
+interface OrderProps {
+  orderData: Order | null;
+  setOrderData: (orderData: Order) => void;
+  paymentPayload: PaymentInitialisePayload | null;
+  setPaymentPayload: (payload: PaymentInitialisePayload) => void;
+  paymentLinkModalOpen: boolean;
+  setPaymentlinkModalOpen: (linkOpen: boolean) => void;
+  // setPaymentlinkModalClose: ()
+}
+
+export const useOrderData = create<OrderProps>((set) => ({
+  orderData: null as Order | null,
+  setOrderData: (orderData) => set({ orderData }),
+  paymentPayload: null as PaymentInitialisePayload | null,
+  setPaymentPayload: (payload) => set({ paymentPayload: payload }),
+  paymentLinkModalOpen: false,
+  setPaymentlinkModalOpen: (linkOpen) =>
+    set({ paymentLinkModalOpen: linkOpen }),
+}));
