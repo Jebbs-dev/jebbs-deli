e22c99c2019307df37ed0c28ea8bfd97ceeba522
Jebbs-dev <ojebiyifulness@gmail.com>
2025-08-15 23:57:25 +0100

    feat: initialise payment transaction on order creation

--- src/modules/checkout/mutations/create-order.ts
+++ src/modules/checkout/mutations/create-order.ts
@@ -2,6 +2,9 @@ import { Cart, Order } from "@/types/types";
 import api from "@/utils/api";
 import { CartItemProps } from "@/store/cart";
 import { useMutation } from "@tanstack/react-query";
+import { useOrderData } from "@/store/order";
+import { useInitialisePayment } from "./initiate-payment";
+import useAuthStore from "@/store/auth";
 
 interface OrderItemProps {
   storeId: string;
@@ -11,6 +14,14 @@ interface OrderItemProps {
 }
 
 export const useCreateOrder = () => {
+  const { setOrderData, setPaymentlinkModalOpen } = useOrderData();
+  const { user } = useAuthStore();
+
+  const {
+    mutateAsync: initialisePayment,
+    isPending: isPaymentInitialisationPending,
+  } = useInitialisePayment();
+
   return useMutation({
     mutationFn: async (data: {
       orderData: Omit<Order, "id">;
@@ -19,7 +30,24 @@ export const useCreateOrder = () => {
       const response = await api.post("/orders", data);
       return response.data;
     },
-    onSuccess: (data) => {},
+    onSuccess: (data) => {
+      console.log(data);
+
+      setOrderData(data);
+
+      setTimeout(() => {
+        initialisePayment({
+          paymentData: {
+            email: String(user?.email),
+            amount: String(data.order.totalPrice * 100),
+          },
+          callback_url: "http://localhost:3000/shop",
+          userId: String(user?.id),
+          orderId: String(data?.order.id),
+          storeId: data?.order.storeId,
+        });
+      }, 500);
+    },
     onError: (error) => {
       console.error("Failed to create order", error);
     },
