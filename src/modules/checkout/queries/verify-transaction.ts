e264fee17526aa54d3016c8468f2dfe834bc13b8
Jebbs-dev <ojebiyifulness@gmail.com>
2025-08-16 23:18:05 +0100

    feat: add query for transaction verification

---
+++ src/modules/checkout/queries/verify-transaction.ts
@@ -0,0 +1,25 @@
+import useAuthStore from "@/store/auth";
+import api from "@/utils/api";
+import { skipToken, useQuery } from "@tanstack/react-query";
+
+export const useVerifyTransaction = (reference: string) => {
+  const { isLoggedIn } = useAuthStore();
+
+  return useQuery({
+    queryKey: ["payment", reference],
+    queryFn: isLoggedIn
+      ? async () => {
+          const response = await api.get(`/payment/verify/${reference}`);
+          return response.data;
+        }
+      : skipToken,
+    enabled: !!reference,
+    refetchInterval: (query) => {
+      const status = query.state.data.status;
+
+      return status && (status === "success" || status === "failed")
+        ? false
+        : 5000;
+    },
+  });
+};
